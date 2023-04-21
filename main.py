e3dcdd3aa4a1abf13ec0647471d0d8548be617b3
Baury Lucas <lucas.baury@efrei.net>
2023-04-21 20:11:22 +0200

    Défilement caméra

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame, sys, time
 from setting import *
 from level import Level
 from player import Player
+from Obstacle import obstacle
+
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -9,16 +11,22 @@ tickRate = pygame.time.Clock()
 level = Level(Lvl_1_map,screen)
 start_time = 0
 t = 0
+obstacle = obstacle()
 
 while 1 and Player !=0  :
+
+    screen.blit(obstacle.image, obstacle.rect)
+    screen.fill('blue')
+    level.run()
+    pygame.display.update()
+    tickRate.tick(60)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
 
-    screen.fill('blue')
-    level.run()
-    pygame.display.update()
-    tickRate.tick(60)
+
 
